eaf479c6771520beb5cdf442af457dc268a4122e
mathiasAla <150154710+mathiasAla@users.noreply.github.com>
2026-09-07 17:29:48 -0300

    Update MsUsuariosApplication.java

--- src/main/java/com/educa/msusuarios/MsUsuariosApplication.java
+++ src/main/java/com/educa/msusuarios/MsUsuariosApplication.java
@@ -1,1 +1,11 @@
+package com.educa.msusuarios;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuariosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsUsuariosApplication.class, args);
+    }
+}
